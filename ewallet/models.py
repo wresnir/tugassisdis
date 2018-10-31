bd5b10ceb399496bcda9d75125aef10408e961d4
wresnir <wresni.ronggowerdhi@gmail.com>
2018-10-31 13:30:57 +0700

    updated

--- ewallet/models.py
+++ ewallet/models.py
@@ -5,7 +5,13 @@ class Repo(models.Model):
     ip = models.CharField(max_length=15)
     npm = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.npm
+
 class User(models.Model):
     user_id = models.CharField(max_length=10, unique=True)
     nama = models.CharField(max_length=50, default='')
     nilai_saldo = models.IntegerField()
+
+    def __str__(self):
+        return self.user_id
--- ewallet/views.py
+++ ewallet/views.py
@@ -68,7 +68,7 @@ def registerView(request):
     if quorum() <= 0.5:
         res['registerReturn'] = QUORUM_NOT_ENOUGH
         return Response(res)
-    try:
+    # try:
         #Register process
         print("before")
         queryset = User(user_id=req['user_id'], nama=req['nama'], nilai_saldo=1000000000)
@@ -76,13 +76,13 @@ def registerView(request):
             queryset.nilai_saldo = 1000000000
         queryset.save()
         print("after")
-        res['registerReturn'] = SUCCESS
-        return Response(res)
-    except Exception as e:
-        #If register process failed
-        print(e)
         res['registerReturn'] = DATABASE_FAILED
         return Response(res)
+    # except Exception as e:
+    #     #If register process failed
+    #     print(e)
+    #     res['registerReturn'] = DATABASE_FAILED
+    #     return Response(res)
     
 
 @csrf_exempt
